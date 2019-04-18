c615fd46d8cc6480c95c8d6270762867e31290c4
remi <remi.pontonnier.38@gmail.com>
2019-04-18 14:21:26 +0200

    Init projet

---
+++ Gaufre/src/Controlleur/Main.java
@@ -0,0 +1,13 @@
+package Controlleur;
+
+import com.sun.javafx.application.LauncherImpl;
+
+import Vue.InterfaceGraphique;
+import javafx.application.Application;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		InterfaceGraphique.launch(InterfaceGraphique.class, args);
+	}
+}
---
+++ Gaufre/src/Vue/InterfaceGraphique.java
@@ -0,0 +1,13 @@
+package Vue;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InterfaceGraphique extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+
+}
